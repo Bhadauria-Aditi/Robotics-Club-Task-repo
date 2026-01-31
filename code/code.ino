268c92ce839f9a99a09a5746a6ec163a9f4c2894
Aditi <aditibhadauria2007@gmail.com>
2026-02-01 02:52:58 +0530

    V1

--- code/code.ino
+++ code/code.ino
@@ -1,29 +1,29 @@
 #define ledpin 8
 #define buttonpin 7
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200); 
-  pinMode(ledpin, OUTPUT); 
-  pinMode(buttonpin, INPUT);
-  digitalWrite(ledpin, LOW); 
+void setup() 
+{
+  Serial.begin(115200);
+  pinMode(ledpin,OUTPUT);
+  pinMode(buttonpin,INPUT);
+  digitalWrite(ledpin,LOW);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
   int in=digitalRead(buttonpin);
 
   if(in==HIGH)
   {
-    digitalWrite(ledpin, HIGH);
-    Serial.println("Led is on"); 
-    delay(1000); 
-    digitalWrite(ledpin, LOW); 
+    digitalWrite(ledpin,HIGH);
+    Serial.println("Led is on");
+    delay(1000);
+    digitalWrite(ledpin,LOW);
     Serial.println("Led is off");
     delay(1500);
   }
   else
   {
-  digitalWrite(ledpin, LOW); 
-  Serial.println("Led is off");
+    digitalWrite(ledpin,LOW);
+    Serial.println("Led is off");
   }
 }
